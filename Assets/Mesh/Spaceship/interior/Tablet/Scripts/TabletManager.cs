0a3cd6bd2a5642d2bd9d13642bd7351b732b5f2e
Evan <evan.labit2@gmail.com>
2024-02-01 11:27:58 +0100

    Added planets on the tablet

--- Assets/Mesh/Spaceship/interior/Tablet/Scripts/TabletManager.cs
+++ Assets/Mesh/Spaceship/interior/Tablet/Scripts/TabletManager.cs
@@ -5,37 +5,34 @@ using UnityEngine.UI;
 
 public class TabletManager : MonoBehaviour
 {
-    public GameObject[] background;
+    public GameObject[] planetsUi;
+    public GameObject[] planetsModels;
     int index;
 
     void Start()
     {
         index = 0;
         setActivePlanet();
-        Debug.Log("actvated");
     }
 
     private void setActivePlanet()
     {
-        for (int i = 0; i < background.Length; i++)
+        for (int i = 0; i < planetsUi.Length; i++)
         {
-            Debug.Log(i == index);
-            background[i].gameObject.SetActive(i == index);
+            planetsUi[i].gameObject.SetActive(i == index);
+            planetsModels[i].gameObject.SetActive(i == index);
         }
     }
 
     public void Next()
     {
-        Debug.Log(index + 1);
-        Debug.Log(background.Length);
-        
-        index = (index + 1) % background.Length;
+        index = (index + 1) % planetsUi.Length;
         setActivePlanet();
     }
 
     public void Previous()
     {
-        index = index - 1 >= 0 ? index - 1 : background.Length - 1;
+        index = index - 1 >= 0 ? index - 1 : planetsUi.Length - 1;
         setActivePlanet();
     }
 }
